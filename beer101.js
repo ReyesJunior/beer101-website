408451913f92dce821abbd897fbf1bdbbedc8f72
Jorge Reyes Jr <reyes.junior.27@gmail.com>
2015-12-31 15:52:46 -0800

    Cleaned up JS code, made it easier to read

--- beer101.js
+++ beer101.js
@@ -1,55 +1,47 @@
-// When Document Loads
-
 $(document).ready(function () {
 
-// Global JS
+/* Menu functions */
 
-// Toggle Mobile menu when clicking mobile-menu-button
+function animateMobileMenu() {
+  $( '.mobile-menu' ).animate( { height: 'toggle' }, 250 );
+}
 
-  $('.mobile-menu-button').click( function() {
-    $('.mobile-menu').animate({height: 'toggle'}, 250, function () {
-    });
-  });
-
-// Toggle Mobile menu when clicking gi-mobile-menu-item
-
-  $('.mobile-menu-item').click( function() {
-    $('.mobile-menu').animate({height: 'toggle'}, 250, function () {
-    });
-  });
-
-// Hide gi-mobile-menu when any anchor is clicked 
-
-$('a').click(function() {
+function hideMobileMenu () {
   $('.mobile-menu').hide();
-});
+}
+
+//Menu Event Bindings
+  $( '.mobile-menu-button' ).on( 'click', animateMobileMenu );
+  $( '.mobile-menu-item' ).on( 'click', animateMobileMenu );
+  $('a').on( 'click', hideMobileMenu );
 
 /* ////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////////////////////////////////// */
+// Toggle hidden-text functions
 
+function revealSectionText () {
+  $( this ).text( 'Read Less' ).siblings( '.complete-text' ).slideToggle();
+}
 
-// Toggle Tap Text cards (Read More/Read Less)
-$('.section-text-button').toggle(function() {
-  // Read more/ Read less button toggle function should act independently from one another when clicked
-  var $this = $(this);
+function hideSectionText () {
+  $( this ).text( 'Read More' ).siblings( '.complete-text' ).slideToggle();
+}
 
-  $this.text('Read Less').siblings('.complete-text').slideToggle();
-  }, function () {
-    $(this).text('Read More').siblings('.complete-text').slideToggle();
-});
+function revealSub_SectionText () {
+  $( this ).text( '<< Read Less' ).siblings( '.complete-text' ).slideToggle();
+}
 
+function hideSub_SectionText () {
+  $( this ).text( 'Read More >>' ).siblings( '.complete-text' ).slideToggle();
+}
 
+// Text-button Event Bindings
+$('.section-text-button').toggle( revealSectionText ,  hideSectionText );
+$('.sub-section-text-button').toggle( revealSub_SectionText ,  hideSub_SectionText );
 
-// Toggle Tap Text cards (Read More/Read Less)
-$('.sub-section-text-button').toggle(function() {
-  // Read more/ Read less button toggle function should act independently from one another when clicked
-  var $this = $(this);
-
-  $this.text('<< Read Less').siblings('.complete-text').slideToggle();
-  }, function () {
-    $(this).text('Read More >>').siblings('.complete-text').slideToggle();
-});
-
+/* ////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////// */
+// Window Resize functions
 
 // On window resize > 800px, move 'Read More/Read Less' button to 2nd paragraph
   $(window).resize(function(){
@@ -76,21 +68,22 @@ $('.sub-section-text-button').toggle(function() {
      }
 }).resize();
 
-    $(window).resize(function(){
-     var width = $(window).width();
+$(window).resize(function(){
+  var width = $(window).width();
      if( width > 1000 ){
         $('.body:nth-child(5)').removeClass('complete-text');
         $('.body:nth-child(5)').removeClass('complete-text');
-        $( '.sub-section-text-button' ).hide();
         $( '.section-text-button' ).hide();
+        $( '.sub-section-text-button' ).hide();
      }
      else {
         $('.body:nth-child(5)').addClass('complete-text');
         $('.body:nth-child(5)').addClass('complete-text');
-        $( '.sub-section-text-button' ).show();
         $( '.section-text-button' ).show();
+        $( '.sub-section-text-button' ).show();
      }
 }).resize();
+
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Brewing-Slider
@@ -109,14 +102,6 @@ $('.gallery-button').on('click', function () {
   var theclass = $(this).hasClass('arrow-right') ? $currentSlide++ : $currentSlide--;
   animate();
 });
-
-
-
-
-
-
-
-
 
 }); // End of $(document).ready function
 
