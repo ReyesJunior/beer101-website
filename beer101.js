5b741766d2d051920252dc1cb79f9cd63a69fd4e
Jorge Reyes Jr <reyes.junior.27@gmail.com>
2015-12-31 11:29:34 -0800

    Added JS code to make the 'text-buttons' move up and dow the 'body':nth-child() as the viewport increases/decreases

--- beer101.js
+++ beer101.js
@@ -38,14 +38,76 @@ $('.section-text-button').toggle(function() {
     $(this).text('Read More').siblings('.complete-text').slideToggle();
 });
 
+
+
 // Toggle Tap Text cards (Read More/Read Less)
 $('.sub-section-text-button').toggle(function() {
   // Read more/ Read less button toggle function should act independently from one another when clicked
   var $this = $(this);
 
-  $this.text('Read Less').siblings('.complete-text').slideToggle();
+  $this.text('<< Read Less').siblings('.complete-text').slideToggle();
   }, function () {
-    $(this).text('Read More').siblings('.complete-text').slideToggle();
+    $(this).text('Read More >>').siblings('.complete-text').slideToggle();
+});
+
+
+// On window resize > 800px, move 'Read More/Read Less' button to 2nd paragraph
+  $(window).resize(function(){
+     var width = $(window).width();
+     if( width > 620 ){
+        $('.body:nth-child(3)').removeClass('complete-text');
+        $('.body:nth-child(3)').removeClass('complete-text');
+     }
+     else {
+        $('.body:nth-child(3)').addClass('complete-text');
+        $('.body:nth-child(3)').addClass('complete-text');
+     }
+  }).resize() // Trigger resize function on page load
+
+    $(window).resize(function(){
+     var width = $(window).width();
+     if( width > 800 ){
+        $('.body:nth-child(4)').removeClass('complete-text');
+        $('.body:nth-child(4)').removeClass('complete-text');
+     }
+     else {
+        $('.body:nth-child(4)').addClass('complete-text');
+        $('.body:nth-child(4)').addClass('complete-text');
+     }
+}).resize();
+
+    $(window).resize(function(){
+     var width = $(window).width();
+     if( width > 1000 ){
+        $('.body:nth-child(5)').removeClass('complete-text');
+        $('.body:nth-child(5)').removeClass('complete-text');
+        $( '.sub-section-text-button' ).hide();
+        $( '.section-text-button' ).hide();
+     }
+     else {
+        $('.body:nth-child(5)').addClass('complete-text');
+        $('.body:nth-child(5)').addClass('complete-text');
+        $( '.sub-section-text-button' ).show();
+        $( '.section-text-button' ).show();
+     }
+}).resize();
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Brewing-Slider
+
+
+var $slideWidth = $('.slide-card').outerWidth(true);
+var slidesN = $('.slider-wrapper').find('.slide-card').length;
+var $currentSlide = 0;   // =  current slide
+
+function animate () { 
+  var $newCurrentSlide = $currentSlide === -1 ? $currentSlide = slidesN - 1 : $currentSlide = $currentSlide % slidesN;
+  $('.slider').stop().animate({left: - ( $slideWidth * $currentSlide ) }, 1000 );  
+}
+
+$('.gallery-button').on('click', function () {
+  var theclass = $(this).hasClass('arrow-right') ? $currentSlide++ : $currentSlide--;
+  animate();
 });
 
 
@@ -54,19 +116,6 @@ $('.sub-section-text-button').toggle(function() {
 
 
 
-
-// On window resize 
-/*  $(window).resize(function(){
-     var width = $(window).width();
-     if(width >= x){
-   
-     }
-     else{
-         
-     }
-  }).resize() // Trigger resize function on page load
-
-*/
 
 
 }); // End of $(document).ready function
