84bd81341bd3e5572fab32dac96880998ac2d543
Jorge Reyes Jr <reyes.junior.27@gmail.com>
2016-05-09 02:06:02 -0500

    add correct and updated html (bootstrap) for almost all for homepage (still requires carousel image text and call to action button)

--- beer101.js
+++ beer101.js
@@ -13,109 +13,136 @@ function hideMobileMenu () {
 // Toggle dropdwon mobile menu
   $( '.dropdown-hamburger' ).on( 'click', toggleMobileMenu );
   $( '.dropdown-menu-button' ).on( 'click', toggleMobileMenu );
-  // $('a').on( 'click', hideMobileMenu );
 
-/* ////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////////////////////////////////////////////// */
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Toggle hidden-text functions
 
 function revealSectionText () {
-  $( this ).text( 'Read Less' ).siblings( '.complete-text' ).slideToggle();
+  $( this ).text( 'Read Less' ).siblings( '.hidden-text' ).slideToggle();
 }
 
 function hideSectionText () {
-  $( this ).text( 'Read More' ).siblings( '.complete-text' ).slideToggle();
-}
-
-function revealSub_SectionText () {
-  $( this ).text( '<< Read Less' ).siblings( '.complete-text' ).slideToggle();
-}
-
-function hideSub_SectionText () {
-  $( this ).text( 'Read More >>' ).siblings( '.complete-text' ).slideToggle();
+  $( this ).text( 'Read More' ).siblings( '.hidden-text' ).slideToggle();
 }
 
 // Text-button Event Bindings
-$('.section-text-button').toggle( revealSectionText ,  hideSectionText );
-$('.sub-section-text-button').toggle( revealSub_SectionText ,  hideSub_SectionText );
+$('.text-visibility-button').toggle( revealSectionText ,  hideSectionText );
 
 
-
-
-/* ////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////////////////////////////////////////////// */
+//////////////////////////////////////////////////////////////////////////////////////////////////////////// 
 // Window Resize functions
+
+//adjust quick-links aside
 
   $(window).resize(function(){
 
     var width = $(window).width();
-      if( width > 767 ){
 
-          $('#collapseOne.panel-collapse.collapse').addClass( 'in' );
+      if( width > 767 ) {
 
         $(window).scroll(function() {    
             var scroll = $(window).scrollTop();
 
-            if (scroll >= 575) {
+            if (scroll >= 755) {
                 $( '.accordion' ).addClass( 'fixed-accordion' );
                 
             } else {
                 $( '.accordion' ).removeClass( 'fixed-accordion' );
-                // $('#collapseOne.panel-collapse.collapse').removeClass( 'in' );
             }
 
           });
 
       } else {
             $( '.accordion' ).removeClass( 'fixed-accordion' );
-            $('#collapseOne.panel-collapse.collapse').removeClass( 'in' );
 
    }
 
   });
 
 // On window resize > 800px, move 'Read More/Read Less' button to 2nd paragraph
+
+// text-visibility-buttons (all but 'beer-descriptors')
+
+function hideAllTextVisibilityButtons() {
+  $('.what-is-beer.text-visibility-button').hide();
+  $('.beer-assessment.text-visibility-button').hide();
+  $('.beer-descriptors.text-visibility-button').hide();
+}
+
+function showAllTextVisibilityButtons() {
+  $('.what-is-beer.text-visibility-button').show();
+  $('.beer-assessment.text-visibility-button').show();
+  $('.beer-descriptors.text-visibility-button').hide();
+}
+//removeCLasses
+function removeFirstHiddenTextClass() {
+  $('.panel-body-text:nth-child(2)').removeClass('hidden-text');
+}
+
+function removeSecondHiddenTextClass() {
+  $('.panel-body-text:nth-child(3)').removeClass('hidden-text');
+}
+
+function removeThirdHiddenTextClass() {
+  $('.panel-body-text:nth-child(4)').removeClass('hidden-text');
+}
+function removeFourthHiddenTextClass() {
+  $('.panel-body-text:nth-child(5)').removeClass('hidden-text');
+}
+
+// addClasses
+
+function addFirstHiddenTextClass() {
+  $('.panel-body-text:nth-child(2)').addClass('hidden-text');
+}
+
+function addSecondHiddenTextClass() {
+  $('.panel-body-text:nth-child(3)').addClass('hidden-text');
+}
+
+
+function addThirdHiddenTextClass() {
+  $('.panel-body-text:nth-child(4)').addClass('hidden-text');
+}
+
+function addFourthHiddenTextClass() {
+  $('.panel-body-text:nth-child(5)').addClass('hidden-text');
+}
+
+
   $(window).resize(function(){
      var width = $(window).width();
-     if( width > 620 ){
-        $('.body:nth-child(3)').removeClass('complete-text');
-        $('.body:nth-child(3)').removeClass('complete-text');
-     }
-     else {
-        $('.body:nth-child(3)').addClass('complete-text');
-        $('.body:nth-child(3)').addClass('complete-text');
-     }
-  }).resize() // Trigger resize function on page load
+        if( width < 620 ){
+          addFirstHiddenTextClass();
+          addSecondHiddenTextClass();
+          addThirdHiddenTextClass();
+          addFourthHiddenTextClass();
+        }
+    }).resize(); // Trigger resize function on page load
+
+  $(window).resize(function(){
+     var width = $(window).width();
+        if( width > 620){
+          removeFirstHiddenTextClass();
+        } else {
+          addFirstHiddenTextClass();
+        }
+    }).resize();
 
     $(window).resize(function(){
-     var width = $(window).width();
-     if( width > 800 ){
-        $('.body:nth-child(4)').removeClass('complete-text');
-        $('.body:nth-child(4)').removeClass('complete-text');
-     }
-     else {
-        $('.body:nth-child(4)').addClass('complete-text');
-        $('.body:nth-child(4)').addClass('complete-text');
-     }
-}).resize();
+       var width = $(window).width();
+        if( width > 820 ){
+          removeSecondHiddenTextClass();
+          removeThirdHiddenTextClass();
+          hideAllTextVisibilityButtons();
+        } else {
+          addSecondHiddenTextClass();
+          addThirdHiddenTextClass();
+          showAllTextVisibilityButtons();
+       }
+    }).resize();
 
-$(window).resize(function(){
-  var width = $(window).width();
-     if( width > 1000 ){
-        $('.body:nth-child(5)').removeClass('complete-text');
-        $('.body:nth-child(5)').removeClass('complete-text');
-        $( '.section-text-button' ).hide();
-        $( '.sub-section-text-button' ).hide();
-     }
-     else {
-        $('.body:nth-child(5)').addClass('complete-text');
-        $('.body:nth-child(5)').addClass('complete-text');
-        $( '.section-text-button' ).show();
-        $( '.sub-section-text-button' ).show();
-     }
-}).resize();
 
-////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Brewing-Slider
 
@@ -133,6 +160,19 @@ $('.gallery-button').on('click', function () {
   var theclass = $(this).hasClass('arrow-right') ? $currentSlide++ : $currentSlide--;
   animate();
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }); // End of $(document).ready function
 
