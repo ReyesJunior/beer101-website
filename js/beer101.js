b87573accf92163a09a5b088e6004bdc5fe75673
Jorge Reyes Jr <reyes.junior.27@gmail.com>
2016-12-12 13:12:22 -0600

    js/beer101.js[ removed js code for toggle 'read more/read/less']

--- js/beer101.js
+++ js/beer101.js
@@ -1,7 +1,8 @@
 
 $(document).ready(function () {
-
-// *** Menu functions *** //
+  
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Menu functions
 
 function toggleMenu() {
   $( '.toggle-menu' ).animate( { height: 'toggle' }, 250 );
@@ -18,163 +19,103 @@ function hideMobileMenu () {
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Toggle hidden-text functions
 
-function revealSectionText () {
-  $( this ).text( 'Read Less' ).siblings( '.hidden-text' ).slideToggle();
-}
+// function revealSectionText () {
+//   $( this ).text( 'Read Less' ).siblings( '.hidden-text' ).slideToggle();
+// }
 
-function hideSectionText () {
-  $( this ).text( 'Read More' ).siblings( '.hidden-text' ).slideToggle();
-}
+// function hideSectionText () {
+//   $( this ).text( 'Read More' ).siblings( '.hidden-text' ).slideToggle();
+// }
 
-// Text-button Event Bindings
-$('.text-visibility-button').toggle( revealSectionText ,  hideSectionText );
+// // Text-button Event Bindings
+// $('.text-visibility-button').toggle( revealSectionText ,  hideSectionText );
 
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////// 
 // Window Resize functions
 
-//adjust quick-links aside
-
-  $(window).resize(function(){
-
-    var width = $(window).width();
-
-      if( width > 767 ) {
-
-        $(window).scroll(function() {    
-            var scroll = $(window).scrollTop();
-
-            if (scroll >= 875) {
-              $( '.quick-links-panel').addClass( 'fixed-quick-links-panel');
-              $( '.content-panels').addClass( 'content-panels-fixed');
-                
-            } else {
-              $( '.quick-links-panel').removeClass( 'fixed-quick-links-panel');
-              $( '.content-panels').removeClass( 'content-panels-fixed');
-            }
-
-          });
-
-      } else {
-        $( '.quick-links-panel').removeClass( 'fixed-quick-links-panel');
-        $( '.content-panels').removeClass( 'content-panels-fixed');
-
-   }
-
-  });
-
 // On window resize > 800px, move 'Read More/Read Less' button to 2nd paragraph
 
 // text-visibility-buttons (all but 'beer-descriptors')
 
-function hideAllTextVisibilityButtons() {
-  $('.what-is-beer.text-visibility-button').hide();
-  $('.beer-assessment.text-visibility-button').hide();
-  $('.beer-descriptors.text-visibility-button').hide();
-}
+// function hideAllTextVisibilityButtons() {
+//   $('.what-is-beer.text-visibility-button').hide();
+//   $('.beer-assessment.text-visibility-button').hide();
+//   $('.beer-descriptors.text-visibility-button').hide();
+// }
 
-function showAllTextVisibilityButtons() {
-  $('.what-is-beer.text-visibility-button').show();
-  $('.beer-assessment.text-visibility-button').show();
-  $('.beer-descriptors.text-visibility-button').show();
-}
-//removeCLasses
-function removeFirstHiddenTextClass() {
-  $('.panel-body-text:nth-child(2)').removeClass('hidden-text');
-}
+// function showAllTextVisibilityButtons() {
+//   $('.what-is-beer.text-visibility-button').show();
+//   $('.beer-assessment.text-visibility-button').show();
+//   $('.beer-descriptors.text-visibility-button').show();
+// }
+// //removeCLasses
+// function removeFirstHiddenTextClass() {
+//   $('.panel-body-text:nth-child(2)').removeClass('hidden-text');
+// }
 
-function removeSecondHiddenTextClass() {
-  $('.panel-body-text:nth-child(3)').removeClass('hidden-text');
-}
+// function removeSecondHiddenTextClass() {
+//   $('.panel-body-text:nth-child(3)').removeClass('hidden-text');
+// }
 
-function removeThirdHiddenTextClass() {
-  $('.panel-body-text:nth-child(4)').removeClass('hidden-text');
-}
-function removeFourthHiddenTextClass() {
-  $('.panel-body-text:nth-child(5)').removeClass('hidden-text');
-}
+// function removeThirdHiddenTextClass() {
+//   $('.panel-body-text:nth-child(4)').removeClass('hidden-text');
+// }
+// function removeFourthHiddenTextClass() {
+//   $('.panel-body-text:nth-child(5)').removeClass('hidden-text');
+// }
 
 // addClasses
 
-function addFirstHiddenTextClass() {
-  $('.panel-body-text:nth-child(2)').addClass('hidden-text');
-}
+// function addFirstHiddenTextClass() {
+//   $('.panel-body-text:nth-child(2)').addClass('hidden-text');
+// }
 
-function addSecondHiddenTextClass() {
-  $('.panel-body-text:nth-child(3)').addClass('hidden-text');
-}
-
-
-function addThirdHiddenTextClass() {
-  $('.panel-body-text:nth-child(4)').addClass('hidden-text');
-}
-
-function addFourthHiddenTextClass() {
-  $('.panel-body-text:nth-child(5)').addClass('hidden-text');
-}
+// function addSecondHiddenTextClass() {
+//   $('.panel-body-text:nth-child(3)').addClass('hidden-text');
+// }
 
 
-  $(window).resize(function(){
-     var width = $(window).width();
-        if( width < 620 ){
-          addFirstHiddenTextClass();
-          addSecondHiddenTextClass();
-          addThirdHiddenTextClass();
-          addFourthHiddenTextClass();
-        }
-    }).resize(); // Trigger resize function on page load
+// function addThirdHiddenTextClass() {
+//   $('.panel-body-text:nth-child(4)').addClass('hidden-text');
+// }
 
-  $(window).resize(function(){
-     var width = $(window).width();
-        if( width > 620){
-          removeFirstHiddenTextClass();
-        } else {
-          addFirstHiddenTextClass();
-        }
-    }).resize();
-
-    $(window).resize(function(){
-       var width = $(window).width();
-        if( width > 820 ){
-          removeSecondHiddenTextClass();
-          removeThirdHiddenTextClass();
-          hideAllTextVisibilityButtons();
-        } else {
-          addSecondHiddenTextClass();
-          addThirdHiddenTextClass();
-          showAllTextVisibilityButtons();
-       }
-    }).resize();
+// function addFourthHiddenTextClass() {
+//   $('.panel-body-text:nth-child(5)').addClass('hidden-text');
+// }
 
 
-////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// Brewing-Slider
+  // $(window).resize(function(){
+  //    var width = $(window).width();
+  //       if( width < 620 ){
+  //         addFirstHiddenTextClass();
+  //         addSecondHiddenTextClass();
+  //         addThirdHiddenTextClass();
+  //         addFourthHiddenTextClass();
+  //       }
+  //   }).resize(); // Trigger resize function on page load
 
+  // $(window).resize(function(){
+  //    var width = $(window).width();
+  //       if( width > 620){
+  //         removeFirstHiddenTextClass();
+  //       } else {
+  //         addFirstHiddenTextClass();
+  //       }
+  //   }).resize();
 
-var $slideWidth = $('.slide-card').outerWidth(true);
-var slidesN = $('.slider-wrapper').find('.slide-card').length;
-var $currentSlide = 0;   // =  current slide
-
-function animate () { 
-  var $newCurrentSlide = $currentSlide === -1 ? $currentSlide = slidesN - 1 : $currentSlide = $currentSlide % slidesN;
-  $('.slider').stop().animate({left: - ( $slideWidth * $currentSlide ) }, 1000 );  
-}
-
-$('.gallery-button').on('click', function () {
-  var theclass = $(this).hasClass('arrow-right') ? $currentSlide++ : $currentSlide--;
-  animate();
-});
-
-
-
-
-
-
-
-
-
-
-
+  //   $(window).resize(function(){
+  //      var width = $(window).width();
+  //       if( width > 820 ){
+  //         removeSecondHiddenTextClass();
+  //         removeThirdHiddenTextClass();
+  //         hideAllTextVisibilityButtons();
+  //       } else {
+  //         addSecondHiddenTextClass();
+  //         addThirdHiddenTextClass();
+  //         showAllTextVisibilityButtons();
+  //      }
+  //   }).resize();
 
 
 
